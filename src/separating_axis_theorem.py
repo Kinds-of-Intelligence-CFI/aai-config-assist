57c57f8bcd55f13c330eeb19a74ebbc8df45c82c
mgm21 <mgm18@ic.ac.uk>
2024-02-19 16:06:11 +0000

    Implemented method to get potential separation axes from rotation value

--- src/separating_axis_theorem.py
+++ src/separating_axis_theorem.py
@@ -4,7 +4,34 @@ from geometry_helper import *
 
 
 # def apply_separating_axis_theorem(rectangle1, rectangle2):
-#     pass
+#     """Returns minimum translation vector for possibly overlapping rectangles 1 and 2"""
+#     # Store the possible axes in a set to avoid checking the same axis twice
+#     possible_separation_axes = set()
+#     for rectangle in [rectangle1, rectangle2]:
+#         ax1, ax2 = get_potential_separation_axes(rectangle.deg_rotation)
+#         possible_separation_axes.add(ax1)
+#         possible_separation_axes.add(ax2)
+
+
+def get_potential_separation_axes(deg_angle):
+    """We will define the separation axes (with all angles in degrees) as:
+       - the axis defined by the rectangle rotation theta
+       - the axis defined by theta + 90 [360]; that's 'modulo 360'
+
+       Indeed, for a rectangle, the SAT algorithm defines the only possible separation axes
+       as the axis defined by its angle of rotation and the axis normal to that one.
+
+       Note, the potential separation axes should be normalised. This is immediately achieved
+       by defining the axes components as the trigonometric function outputs, as done below"""
+
+    rad_angle1 = np.deg2rad(deg_angle)
+    rad_angle2 = np.deg2rad((deg_angle + 90) % 360)
+
+    # Note the axes are returned as tuples to be hashable and hence inputtable into a set
+    ax1 = (np.cos(rad_angle1), np.sin(rad_angle1))
+    ax2 = (np.cos(rad_angle2), np.sin(rad_angle2))
+
+    return ax1, ax2
 
 
 class Rectangle:
@@ -32,6 +59,14 @@ if __name__ == "__main__":
     rotation2 = 45
     rectangle2 = Rectangle(center2, width2, height2, rotation2)
     print(rectangle2.vertices)
+
+    # Determine the possible separation axes
+    possible_separation_axes = set()
+    for rectangle in [rectangle1, rectangle2]:
+        ax1, ax2 = get_potential_separation_axes(rectangle.deg_rotation)
+        possible_separation_axes.add(ax1)
+        possible_separation_axes.add(ax2)
+    print(possible_separation_axes)
 
     # apply_separating_axis_theorem()
     print("Exit ok")
