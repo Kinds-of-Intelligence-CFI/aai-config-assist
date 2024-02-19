cd796fda37cca48d4f5dcfc1daaf5aeebfc45b44
mgm21 <mgm18@ic.ac.uk>
2024-02-19 12:00:41 +0000

    Edited 2d point rotation method to support vectorised inputs

--- src/geometry_helper.py
+++ src/geometry_helper.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+# def calculate_vertices_of_rotated_rectangle(centroid, width, height, angle_deg):
+#     vertices = calculate_vertices_of_axis_aligned_rectangle(centroid, width, height)
+#
 
 def calculate_vertices_of_axis_aligned_rectangle(centroid, width, height):
     """Returns the coordinates of the vertices a, b, c, d (top left, top right, bottom right, bottom left)
@@ -13,12 +16,14 @@ def calculate_vertices_of_axis_aligned_rectangle(centroid, width, height):
     c = centroid + np.array([half_width, -half_height])
     d = centroid + np.array([-half_width, -half_height])
 
-    return a, b, c, d
+    vertices = np.array([a, b, c, d])
+
+    return vertices
 
 
-def calculate_clockwise_rotated_2d_point(point, angle_deg):
-    """Returns the new coordinates of a point, rotated by a clockwise angle in degrees
-       point: original coordinates, numpy array 2x1
+def calculate_clockwise_rotated_2d_point(points, angle_deg):
+    """Returns the new coordinates of points(s), rotated by a clockwise angle in degrees
+       points: original coordinates, numpy array nx2 with n, the number of points
        angle_deg: int/float expressed in degrees"""
 
     angle = np.deg2rad(angle_deg)
@@ -29,23 +34,25 @@ def calculate_clockwise_rotated_2d_point(point, angle_deg):
         [-np.sin(angle), np.cos(angle)]
     ])
 
-    rotated_point = np.matmul(rotation_mat, point)
+    rotated_point = np.matmul(rotation_mat, points.T)
 
-    return rotated_point
+    return rotated_point.T
 
 
 if __name__ == "__main__":
-    # Rotating a point in a 2d plane
-    original_point = np.array([2, 2])
+    # Rotating a points in a 2d plane
+    original_point = np.array([[-2, 2], [2, 2], [2, -2], [-2, -2]])
     angle_degrees = 90
-    new_point = calculate_clockwise_rotated_2d_point(point=original_point, angle_deg=angle_degrees)
-    print(new_point)
+    new_points = calculate_clockwise_rotated_2d_point(points=original_point, angle_deg=angle_degrees)
+    print(np.shape(new_points))
+    print(new_points)
 
     # Calculating the vertices of an axis-aligned rectangle from centroid, width, and height
     centroid1 = np.array([0, 0])
     height1 = 2
     width1 = 2
     vertices1 = calculate_vertices_of_axis_aligned_rectangle(centroid=centroid1, width=width1, height=height1)
+    print(np.shape(vertices1))
     print(vertices1)
 
 
