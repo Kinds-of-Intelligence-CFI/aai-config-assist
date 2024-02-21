ad43970f20e4ed586aa1acb63c18801607c7c2ad
mgm21 <mgm18@ic.ac.uk>
2024-02-21 18:01:26 +0000

    Added a short script for users to check and visualise their configs

---
+++ scripts/checking_and_visualising.py
@@ -0,0 +1,9 @@
+from src.config_assistant import ConfigAssistant
+
+if __name__ == "__main__":
+    # Change config_path to the path to your config.yaml file (path relative to this script)
+    config_path = "../example_configs/config.yaml"
+
+    config_assistant = ConfigAssistant(config_path)
+    config_assistant.check_overlap()
+    config_assistant.visualise_config()
