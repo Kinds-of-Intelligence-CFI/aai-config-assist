33f81107c7dc3dfe9047f5569e9c968bf9fc9def
mgm21 <mgm18@ic.ac.uk>
2024-05-09 11:49:23 +0100

    Combined YAML construction methods into one in ArenaConfigLoader

--- src/processing/arena_config_loader.py
+++ src/processing/arena_config_loader.py
@@ -9,30 +9,14 @@ class ArenaConfigLoader(yaml.SafeLoader):
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.add_constructor('!Arena', self.construct_arena)
-        self.add_constructor('!ArenaConfig', self.construct_arena_config)
-        self.add_constructor('!Item', self.construct_item)
-        self.add_constructor('!Vector3', self.construct_vector3)
-        self.add_constructor('!RGB', self.construct_rgb)
+        self.add_constructor('!Arena', self._construct_tag)
+        self.add_constructor('!ArenaConfig', self._construct_tag)
+        self.add_constructor('!Item', self._construct_tag)
+        self.add_constructor('!Vector3', self._construct_tag)
+        self.add_constructor('!RGB', self._construct_tag)
 
     @staticmethod
-    def construct_arena(loader: yaml.Loader, node: yaml.MappingNode) -> Dict[Hashable, Any]:
-        return loader.construct_mapping(node)
-
-    @staticmethod
-    def construct_arena_config(loader: yaml.Loader, node: yaml.MappingNode) -> Dict[Hashable, Any]:
-        return loader.construct_mapping(node)
-
-    @staticmethod
-    def construct_item(loader: yaml.Loader, node: yaml.MappingNode) -> Dict[Hashable, Any]:
-        return loader.construct_mapping(node)
-
-    @staticmethod
-    def construct_vector3(loader: yaml.Loader, node: yaml.MappingNode) -> Dict[Hashable, Any]:
-        return loader.construct_mapping(node)
-
-    @staticmethod
-    def construct_rgb(loader: yaml.Loader, node: yaml.MappingNode) -> Dict[Hashable, Any]:
+    def _construct_tag(loader: yaml.Loader, node: yaml.MappingNode) -> Dict[Hashable, Any]:
         return loader.construct_mapping(node)
 
 
